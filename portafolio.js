eb49f0f2977681c043cab6f524e631510b9a67b6
Cristian Damil <cristiancode2023@gmail.com>
2023-07-11 18:35:56 +0200

    delete version without Astro

--- portafolio.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.logo').addEventListener('click', function(event) {
-    var cssVariable = getComputedStyle(this).getPropertyValue('--_i').trim();
-    if (cssVariable !== '0%') {
-        event.preventDefault();
-    }
-});
